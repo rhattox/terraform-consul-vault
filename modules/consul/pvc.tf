def1b380157be271000f7e9948453daf2c5ce6e9
Bruno Coviello <bcovies@gmail.com>
2024-03-18 22:54:32 -0300

    Fix PVC

--- modules/consul/pvc.tf
+++ modules/consul/pvc.tf
@@ -1,19 +1,19 @@
-# resource "kubernetes_persistent_volume_claim" "vault_pvc" {
-#  depends_on = [ 
-#     kubernetes_persistent_volume.consul_pv
-#   ]
-#   metadata {
-#     name = "consul-pvc"
-#     namespace = "consul"
-#   }
-#   spec {
-#     access_modes = ["ReadWriteMany"]
-#     resources {
-#       requests = {
-#         storage = "10Gi"
-#       }
-#     }
-#     storage_class_name = "consul-local-storage"
-#     volume_name = "${kubernetes_persistent_volume.consul_pv.metadata.0.name}"
-#   }
-# }
+resource "kubernetes_persistent_volume_claim" "vault_pvc" {
+ depends_on = [ 
+    kubernetes_persistent_volume.consul_pv
+  ]
+  metadata {
+    name = "data-consul-consul-server-0"
+    namespace = "consul"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+    storage_class_name = "consul-local-storage"
+    volume_name = "${kubernetes_persistent_volume.consul_pv.metadata.0.name}"
+  }
+}
